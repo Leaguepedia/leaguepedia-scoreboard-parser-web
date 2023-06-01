9669d2a068ae339e511c035d04f6fd09641fdc20
Santiago Kozak <santikzk1406@gmail.com>
2023-06-01 03:43:04 -0300

    Use wiki as a mirror

---
+++ static/js/1.5/main.js
@@ -0,0 +1,152 @@
+var jqxhr = {abort: function () {}};
+var currentQueryId = null;
+
+function printOutput(response) {
+    $('#output').html("");
+    if (response.success === false) {
+        $("#output").html("An error has occured:");
+        printExceptions(response.errors);
+        return;
+    }
+    let el = document.createElement('textarea');
+	$(el).attr('readonly', '')
+		.attr('id', 'output-scoreboard')
+		.val(response.text);
+    $(el).appendTo($('#output'));
+    el.select();
+    printExceptions(response.warnings);
+}
+
+function printExceptions(exceptions) {
+    if (exceptions == null) {
+        return;
+    }
+    let warnings = document.createElement("div");
+    $(warnings).attr("id", "output-warnings");
+    exceptions.forEach(function (item) {
+        let el = document.createElement('div');
+        $(el).html(item);
+        $(el).appendTo($(warnings));
+    });
+    $(warnings).appendTo($("#output"));
+}
+
+function waitForResponse(queryId, callback) {
+    currentQueryId = queryId;
+    var i = 0;
+    var interval = setInterval(function(queryId) {
+        if (currentQueryId === queryId) {
+            i = i + 1
+            $.ajax({
+                type: "GET",
+                url: "/parser/query",
+                data: {
+                    queryId: queryId,
+                    iteration: i,
+                },
+                success: function(response) {
+                    if (response.ready === true) {
+                        clearInterval(interval);
+                        callback(response.payload);
+                    } else {
+                        $("#output").html("Doing parser magic...<br>Parsed matches " + response.nParsedMatches + "/" + response.totalMatches);
+                    }
+                },
+                error: function() {
+                    $("#output").html("An error has occured! Please try again.");
+                    clearInterval(interval);
+                    callback(null);
+                },
+            });
+        } else {
+            clearInterval(interval);
+        };
+    }, 5000, queryId);
+}
+
+function getCookieExpiration() {
+    const d = new Date();
+    return d.toUTCString(d.setTime(d.getTime() + (3650*24*60*60*1000)));
+}
+
+$(document).ready(function(){
+    $('#parser-form').submit(function(e) {
+        e.preventDefault();
+        let source = $("input[name='source']:checked").val();
+        let expires = getCookieExpiration();
+        document.cookie = `prefsSource=${source}; expires=${expires};`
+        jqxhr.abort();
+        jqxhr = $.ajax({
+            type: 'POST',
+            url: '/parser/query',
+            data: {
+                ids: $("#ids").val(),
+                source: source,
+                header: $("input[name='header']:checked").val(),
+                skipQueries: $("input[name='skip-queries']:checked").val(),
+                useWikiMirror: $("input[name='wiki-mirror']:checked").val(),
+            },
+            beforeSend: function() {
+                $("#output").html("Doing parser magic...<br>Parsed matches 0/?");
+            },
+            success: function(response) {
+                if (response.errors) {
+                    printOutput(response);
+                    return;
+                }
+                $("#output").html("Doing parser magic...<br>Parsed matches 0/" + response.totalMatches);
+                waitForResponse(response.queryId, function(payload) {
+                    if (payload) {
+                        printOutput(payload);
+                    }
+                });
+            },
+            error: function() {
+                $("#output").html("An error has occured! Please try again.");
+            }
+        });
+    });
+
+    $('#clear-cache').click(function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: 'POST',
+            url: '/clearcache',
+            beforeSend: function() {
+                $("#output").html("Clearing cache...");
+            },
+            success: function() {
+                $("#output").html("Done!");
+            },
+            error: function() {
+                $("#output").html("An error has occured! Please try again.");
+            }
+        });
+    });
+
+    $('#clear-prefs').click(function() {
+        document.cookie = "prefsSource=; expires=Thu, 01 Jan 1970 00:00:00 UTC;"
+        document.cookie = "theme=; expires=Thu, 01 Jan 1970 00:00:00 UTC;"
+        $("#riot").prop('checked', true);
+        $("#riot-live").prop('checked', false);
+        $("#qq").prop('checked', false);
+        $("body").removeClass("theme-light").addClass("theme-dark");
+    });
+
+    $(".toggle-theme").click(function(e) {
+        e.preventDefault()
+        e.stopPropagation()
+        var currentTheme = $("body").attr("class");
+        if (currentTheme == "theme-dark") {
+            var newTheme = "light"
+            $("body").removeClass("theme-dark").addClass("theme-light");
+        } else if (currentTheme == "theme-light") {
+            var newTheme = "dark"
+            $("body").removeClass("theme-light").addClass("theme-dark");
+        }
+
+        let expires = getCookieExpiration();
+
+        document.cookie = `theme=${newTheme}; expires=${expires}`;
+    });
+});
